8971b7ae99e508fc31719b3199af5ce26819db5b
JuniorMeloDev <juninhotj_@hotmail.com>
2025-08-03 11:40:21 -0300

    Correções api Lançamentos2

--- src/app/api/lancamentos/route.js
+++ src/app/api/lancamentos/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { supabase } from '@/utils/supabaseClient';
+import { supabase } from '@/app/utils/supabaseClient';
 import jwt from 'jsonwebtoken';
 
 // POST: Cria um novo lançamento manual (Débito, Crédito ou Transferência)
