f10eaa24f4b817af2ead8ef38838929030882441
JuniorMeloDev <juninhotj_@hotmail.com>
2025-08-15 00:55:07 -0300

    Melhorias na extração de pdf para o cte5

--- src/app/api/upload/cte-pdf/route.js
+++ src/app/api/upload/cte-pdf/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { supabase } from '@/utils/supabaseClient';
+import { supabase } from '@/app/utils/supabaseClient';
 import jwt from 'jsonwebtoken';
 import PDFParser from "pdf2json"; 
 
